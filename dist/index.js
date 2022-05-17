69cbe7ab722c1dc37b28a0d489b2efc5936f88b6
akoneko <akoneko47@gmail.com>
2022-05-17 12:23:44 +0700

    1.3.21

--- dist/index.js
+++ dist/index.js
@@ -347,12 +347,19 @@ var Component = {
     closeOnClick: {
       type: Boolean,
       default: true
+    },
+
+    pauseOnHover: {
+      type: Boolean,
+      default: false
     }
+
   },
   data: function data() {
     return {
       list: [],
-      velocity: __WEBPACK_IMPORTED_MODULE_0__index__["default"].params.velocity
+      velocity: __WEBPACK_IMPORTED_MODULE_0__index__["default"].params.velocity,
+      timerControl: ""
     };
   },
   mounted: function mounted() {
@@ -401,14 +408,26 @@ var Component = {
   },
   methods: {
     destroyIfNecessary: function destroyIfNecessary(item) {
+      this.$emit('click', item);
       if (this.closeOnClick) {
         this.destroy(item);
+      }
+    },
+    pauseTimeout: function pauseTimeout() {
+      if (this.pauseOnHover) {
+        this.timerControl.pause();
+      }
+    },
+    resumeTimeout: function resumeTimeout() {
+      if (this.pauseOnHover) {
+        this.timerControl.resume();
       }
     },
     addItem: function addItem(event) {
       var _this = this;
 
       event.group = event.group || '';
+      event.data = event.data || {};
 
       if (this.group !== event.group) {
         return;
@@ -444,9 +463,9 @@ var Component = {
       };
 
       if (duration >= 0) {
-        item.timer = setTimeout(function () {
-          _this.destroy(item);
-        }, item.length);
+        this.timerControl = new __WEBPACK_IMPORTED_MODULE_2__util__["c" /* Timer */](function () {
+          return _this.destroy(item);
+        }, item.length, item);
       }
 
       var direction = this.reverse ? !this.botToTop : this.botToTop;
@@ -495,6 +514,8 @@ var Component = {
       if (!this.isVA) {
         this.clean();
       }
+
+      this.$emit('destroy', item);
     },
     destroyById: function destroyById(id) {
       var item = this.list.find(function (v) {
@@ -658,6 +679,7 @@ var parse = function parse(value) {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return Id; });
 /* unused harmony export split */
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return listToDirection; });
+/* harmony export (immutable) */ __webpack_exports__["c"] = Timer;
 var directions = {
   x: ['left', 'center', 'right'],
   y: ['top', 'bottom']
@@ -697,6 +719,24 @@ var listToDirection = function listToDirection(value) {
   });
 
   return { x: x, y: y };
+};
+
+function Timer(callback, delay, notifItem) {
+  var start = void 0,
+      remaining = delay;
+
+  this.pause = function () {
+    clearTimeout(notifItem.timer);
+    remaining -= Date.now() - start;
+  };
+
+  this.resume = function () {
+    start = Date.now();
+    clearTimeout(notifItem.timer);
+    notifItem.timer = setTimeout(callback, remaining);
+  };
+
+  this.resume();
 };
 
 /***/ }),
@@ -847,25 +887,31 @@ module.exports={render:function (){var _vm=this;var _h=_vm.$createElement;var _c
       style: (_vm.notifyWrapperStyle(item)),
       attrs: {
         "data-id": item.id
-      }
-    }, [_vm._t("body", [_c('div', {
-      class: _vm.notifyClass(item),
+      },
       on: {
-        "click": function($event) {
-          return _vm.destroyIfNecessary(item)
+        "mouseenter": _vm.pauseTimeout,
+        "mouseleave": _vm.resumeTimeout
+      }
+    }, [_vm._t("body", function() {
+      return [_c('div', {
+        class: _vm.notifyClass(item),
+        on: {
+          "click": function($event) {
+            return _vm.destroyIfNecessary(item)
+          }
         }
-      }
-    }, [(item.title) ? _c('div', {
-      staticClass: "notification-title",
-      domProps: {
-        "innerHTML": _vm._s(item.title)
-      }
-    }) : _vm._e(), _vm._v(" "), _c('div', {
-      staticClass: "notification-content",
-      domProps: {
-        "innerHTML": _vm._s(item.text)
-      }
-    })])], {
+      }, [(item.title) ? _c('div', {
+        staticClass: "notification-title",
+        domProps: {
+          "innerHTML": _vm._s(item.title)
+        }
+      }) : _vm._e(), _vm._v(" "), _c('div', {
+        staticClass: "notification-content",
+        domProps: {
+          "innerHTML": _vm._s(item.text)
+        }
+      })])]
+    }, {
       "item": item,
       "close": function () { return _vm.destroy(item); }
     })], 2)
